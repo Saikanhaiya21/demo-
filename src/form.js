bed7f3a2c3757ce0fc2aecd73ce42303bef2d6a0
Sai-Git <saikanhaiya36@gmail.com>
2022-09-06 15:43:21 +0530

    change in form

--- src/form.js
+++ src/form.js
@@ -2,12 +2,7 @@ import './form.css'
 
 let Form = ()=>{
     return (
-        <>
-            <button type="submit" className="button">Add Data</button>
-            Name:<input type="text" id="name" placeholder="Enter name"/>
-            Email:<input type="text" id="name" placeholder="Enter email"/>
-            Contact:<input type="text" id="name" placeholder="Enter contact"/>
-        </>
+      
     )
 }
 export default Form;
